f73a4dcb5f953acde85a3793a3e7c90db7787f53
Jim <Jimschel1@outlook.com>
2026-01-06 09:22:33 +0100

    Update lastUpdate date and message in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // HIER PAS JE ALLEEN DEZE DATUM AAN!
 // ============================================
 const SITE_CONFIG = {
-    lastUpdate: "18 december 2025. Kerstpagina toegevoegd.",  // <-- VERANDER ALLEEN DIT!
+    lastUpdate: "6 Januari 2025. Kerstpagina verwijderd.",  // <-- VERANDER ALLEEN DIT!
     siteName: "BBA Klassenwebsite",
     currentYear: new Date().getFullYear(),
     email: "Jimschel1@outlook.com"
